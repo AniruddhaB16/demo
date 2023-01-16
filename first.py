6f778670aedf57d1067c0f14cd3a84c60485cf90
AniruddhaB16 <anyabelokar.16@gmail.com>
2023-01-17 00:00:53 +0530

    Burger

--- first.py
+++ first.py
@@ -1,8 +1,11 @@
 hungry=input("Are you hungry??")
 if hungry=="yes":
     print("Eat samosa")
+<<<<<<< HEAD
     print("Eat Maggii")
     print("Eat Burger")
+=======
+>>>>>>> parent of 7499bf2 (Maggii)
 else:
     print("Study")
  
